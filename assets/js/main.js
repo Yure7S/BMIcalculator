c49dbb737cfd7774a3fb86e3b64e46d38dc07a26
Yure Silva <yuresilvaesilva07@gmail.com>
2022-12-14 17:19:48 -0300

    v1

---
+++ assets/js/main.js
@@ -0,0 +1,66 @@
+// Calculo de IMC
+
+/* 
+    Captura de elementos
+    Validação de dados dos inputs (terá que retornar true ou false)
+    função para cálculo de IMC
+    função para printar o resultado na tela
+*/
+
+function scope(){
+    let div = document.querySelector("#resultado")
+    let formulario = document.querySelector("#formulario")
+    let p = document.querySelector("#p")
+    let a = document.querySelector("#a")
+
+    // Validando dados dos inputs
+    function validatingData(pv, av){
+        if(pv != "" && av != ""){
+            return true
+        } else{
+            return false
+        }
+    }
+
+    // Calculando o IMC
+    function calcImc(pv, av){
+        let imc = pv / (av ** 2)
+        let resultado
+
+        if(imc < 18.5){
+            resultado = "Abaixo do peso"
+        } else if(imc >= 18.5 && imc <=24.99){
+            resultado = "Peso normal"
+        } else if(imc >= 25 && imc <= 29.99){
+            resultado = "Sobrepeso"
+        }  else if(imc >= 30 && imc <= 34.99){
+            resultado = "Obesidade grau 1"
+        } else if(imc >= 35 && imc <= 39.99){
+            resultado = "Obesidade grau 2"
+        } else{
+            resultado = "Obesidade grau 3"
+        }
+
+        return resultado
+    }
+
+    // Criando resultado no DOM
+    function addResult(bl){
+        imc = p.value / (a.value ** 2)
+
+        if(bl == true){
+            div.classList = "resultado-s"
+            div.textContent = `Seu IMC é ${imc.toFixed(2)} (${calcImc(p.value, a.value)})`
+        } else{
+            div.classList = "resultado-n"
+            div.textContent = `Dados inválidos!`
+        }
+    }
+
+    // Atribuindo ação ao submit
+    formulario.addEventListener("submit", (e) => {
+        e.preventDefault()
+        addResult(validatingData(p.value, a.value))
+    })
+}
+scope()
